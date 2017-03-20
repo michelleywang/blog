e63bb34977f3bd20acd515f90267886fe8205cdf
Michelle Wang <michellewang@d-172-25-231-36.dhcp.virginia.edu>
2017-03-20 12:07:35 -0400

    cleaning directory

--- wordpress/wp-content/themes/theme/functions.php
+++
@@ -1,5 +0,0 @@
-<?php
-add_theme_support( 'post-thumbnails' );
-set_post_thumbnail_size( 50, 50);
-add_post_type_support( 'page', 'excerpt' );
-?>
--- wordpress/wp-content/themes/tiledfourteen-master/inc/updater.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'theme-updates/theme-update-checker.php';
-new ThemeUpdateChecker(
-    'tiledfourteen',
-    'https://remaintenance.io/updates/?action=get_metadata&slug=tiledfourteen'
-);
